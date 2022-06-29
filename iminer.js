ab33ef2d616249c575137b3432ec9e19ab7c6949
followgithub <followtherabbit@gmail.com>
2022-06-29 12:09:36 +0800

    Update iminer.js

--- iminer.js
+++ iminer.js
@@ -1,4 +1,3 @@
-console.log("from git----------------------")
 var __otherApiUrl = {url:["https://api.iminer.info/api/passport/login", "http://13.212.191.109/api/passport/login"]}
 if (window.testApiDomain) {
     __otherApiUrl.url.forEach(a => window.testApiDomain(a))
